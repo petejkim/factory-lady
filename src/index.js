a877b673f0a40727654412a9dd7823a2b0d83003
Chetan Verma <chetan.verma@talentica.com>
2016-06-09 16:49:52 +0530

    o Added few TODO items to index.js o Updated README as per new API and to be more concise, pointing to full API docs that needs to be done.

--- src/index.js
+++ src/index.js
@@ -7,3 +7,8 @@ import FactoryGirl from './FactoryGirl';
 const factory = new FactoryGirl();
 
 export default factory;
+
+// TODO: implement afterBuild, afterCreate options
+// TODO: Object adapter still works with models. Either change that or,
+//       add a new JSONAdapter
+// TODO: Allow creating new FactoryGirl objects
